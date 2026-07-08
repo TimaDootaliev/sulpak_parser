ca3838f1167ad7a4fd76e0281f086cd8c031f106
tima <timur.dootaliev.td@gmail.com>
2026-07-08 20:04:22 +0600

    parse by category added, codel cleaned

--- main.py
+++ main.py
@@ -7,10 +7,6 @@ from bs4._typing import _SomeTags
 from tqdm import tqdm
 
 
-BASE_URL = 'https://www.sulpak.kg'
-
-# https://www.sulpak.kg/f/smartfoniy?page=
-
 """
 1. Получить html-код страницы
 2. Из полученного html-кода отфильтровать данные:
@@ -19,6 +15,9 @@ BASE_URL = 'https://www.sulpak.kg'
 3. Полученные привести в вид словаря
 4. Сохранить данные в json/csv
 """
+
+
+BASE_URL = 'https://www.sulpak.kg'
 
 def get_html(url: str, page_number: int = 1) -> str:
     response = requests.get(url, params={'page': page_number})
@@ -31,32 +30,24 @@ def get_soup(html: str) -> BeautifulSoup:
     return BeautifulSoup(html, 'html.parser')
 
 
-
 def get_cards(soup: BeautifulSoup, tag: str, class_: str) -> _SomeTags:
     cards = soup.find_all(tag, {'class': class_})
-    return cards  # [<div>some info</div>, ]
+    return cards
 
-
-
-
-# print(cards)
-
-# phone = cards[0]
-# print(phone.find('div', {'class': 'product__item-name'}).text)
-
-class_names = {
-    'name': 'product__item-name',
-    'price': 'product__item-price',
-    'description': 'product__item-description',
-    'in_stock': 'product__item-showcase',
-    # 'reviews_count' , tag - span
-}
 
 def parse_cards(cards: _SomeTags) -> list[dict]:
+    class_names = {
+        "name": "product__item-name",
+        "price": "product__item-price",
+        "description": "product__item-description",
+        "in_stock": "product__item-showcase",
+        "reviews": "product__item-reviews"
+    }
+
     def find_info(card: Tag, tag='div', attr=None) -> str:
         if attr:
-            return card.find(tag, {'class': attr}).text # empty_card.text
-        return card.find(tag).text
+            return card.find(tag, {'class': attr}).get_text(strip=True) # empty_card.text
+        return card.find(tag).get_text(strip=True)
 
     result = []
     for card in cards:
@@ -65,28 +56,28 @@ def parse_cards(cards: _SomeTags) -> list[dict]:
             price = find_info(card, attr=class_names['price'])
             description = find_info(card, attr=class_names['description'])
             in_stock = find_info(card, attr=class_names['in_stock'])
-            reviews = find_info(card, 'span')
+            reviews = find_info(card, attr=class_names['reviews'])
         except AttributeError:
             continue
         else:
             data = {
-                'name': name.strip(),
+                'name': name,
                 'price': int(price.replace('сом', '').replace(' ', '').strip()),
-                'description': description.strip(),
-                'in_stock': in_stock.strip(),
-                'reviews': reviews.strip()
+                'description': description,
+                'in_stock': in_stock,
+                'reviews': reviews
             }
             result.append(data)
     return result
 
 
-def save_to_json(data: list[dict]) -> None:
-    with open('data/data.json', 'w') as file:
+def save_to_json(data: list[dict], filename: str) -> None:
+    with open(f'data/{filename}.json', 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 
-def save_to_csv(data: list[dict]) -> None:
-    with open('data/data.csv', 'w') as file:
+def save_to_csv(data: list[dict], filename: str) -> None:
+    with open(f'data/{filename}.csv', 'w') as file:
         fieldnames = ('name', 'price', 'description', 'in_stock', 'reviews')
         writer = csv.DictWriter(file, fieldnames=fieldnames)
 
@@ -102,7 +93,7 @@ def parse_by_page(page_number, category):
     for page in range(1, page_number):  # (1, 2, 3, 4)
         html = get_html(BASE_URL + category, page)
         soup = get_soup(html)
-        cards_from_one_page = get_cards(soup, tag, class_) # [card1, card2]
+        cards_from_one_page = get_cards(soup, tag, class_)
         all_cards.extend(cards_from_one_page)
 
     parsed_data = parse_cards(all_cards)
@@ -120,27 +111,20 @@ def get_categories(soup: BeautifulSoup) -> list[str]:
 
     return links
 
-# parsed_data = parse_by_page(2, 'f/piylesosiy')  # TODO: fix first page (page=1)
-
-# print(len(parsed_data))
-
-# save_to_json(parsed_data)
-# save_to_csv(parsed_data)
-
 
 def parse_by_category(categories: list[str]) -> list[dict]:
-    all_results = []
-    for category in categories:
-        parsed_data = parse_by_page(2, category)
-        all_results.extend(parsed_data)
+    category_names = get_categories_names(categories)
+    for category, category_name in zip(categories, category_names):
+        parsed_data = parse_by_page(2, category)  # TODO: parse max page from sulpak and pass it to page number
+        save_to_csv(parsed_data, category_name)
+        save_to_json(parsed_data, category_name)
 
-    return all_results
+
+def get_categories_names(categories: list[str]) -> list[str]:
+    return [category.split('/')[2] for category in categories]
 
 
 html = get_html(BASE_URL)
 soup = get_soup(html)
 categories = get_categories(soup)
-
-parsed_data = parse_by_category(categories)
-save_to_json(parsed_data)
-save_to_csv(parsed_data)
+parse_by_category(categories)
